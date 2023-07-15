fba49ebd025e7464bb2b132b916702bfa5029262
Surya Varman <suryavarmane@gmail.com>
2023-07-16 00:49:50 +0530

    user registration system

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from users.forms import UserRegisterForm
+from django.http import HttpResponse
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return HttpResponse("Created Successfully")
+        else:
+            return render(request, 'users/register.html', {'form': form})
+    else:
+        form = UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form})
+
